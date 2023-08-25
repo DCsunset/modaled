cc1212a893e6bc5e668ad24589cfb175a8069304
DCsunset <DCsunset@protonmail.com>
2023-08-25 12:27:24 -0400

    chore: add more type output in changelog

--- .versionrc.js
+++ .versionrc.js
@@ -19,6 +19,17 @@ const trackers = [
 ];
 
 module.exports = {
+  types: [
+    {"type": "feat", "section": "Features"},
+    {"type": "fix", "section": "Bug Fixes"},
+    {"type": "chore", "section": "Misc"},
+    {"type": "docs", "section": "Misc"},
+    {"type": "style", "section": "Misc"},
+    {"type": "refactor", "section": "Misc"},
+    {"type": "perf", "section": "Misc"},
+    {"type": "test", "section": "Misc"},
+    {"type": "ci", "section": "Misc"}
+  ],
 	// read version
 	packageFiles: trackers,
 	// write version
